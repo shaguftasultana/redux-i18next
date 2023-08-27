8e814ecb9496f2c9bc4e051da6bc0fd97e2dbbec
Shagufta Sultana <shaguftaahm5560@gmail.com>
2023-08-27 21:08:16 +0500

    2nd

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -8,73 +8,7 @@ const Contact = () => {
     <div>
       <Box className="mx-10 my-20 ">
         <h1 className="text-5xl font-Bold my-5">{t("contact")}</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
-          soluta quaerat dolor rem voluptatem distinctio corrupti, quos veniam
-          blanditiis velit amet molestias facere sit. Architecto sequi natus
-          officia provident modi atque ducimus ullam fugiat, voluptatibus cumque
-          eaque iusto blanditiis voluptate minima deleniti consequuntur
-          reiciendis nihil praesentium quasi, magni aperiam sapiente incidunt.
-          Officiis, harum velit! Dolore, odit ea. Numquam rem velit eos
-          voluptates suscipit quasi ab quaerat et necessitatibus? Laboriosam
-          voluptas blanditiis, rem accusantium maiores, cum voluptates aperiam
-          similique sunt culpa inventore tempore. Obcaecati exercitationem
-          molestiae molestias architecto quia sunt accusantium beatae fugit,
-          consectetur illo impedit iure asperiores vitae eaque ratione id neque
-          ad. Error facere aliquid placeat quam perspiciatis repellat ipsum
-          voluptatibus dolorum mollitia quo molestias aliquam blanditiis
-          voluptate vel, tempore eum? Vero animi expedita quis neque
-          consequuntur pariatur minima exercitationem similique voluptatem, ad
-          inventore debitis ratione cupiditate voluptas harum impedit repellat.
-          Dolore iste dolorem eligendi dicta, neque ipsum labore porro
-          architecto, possimus adipisci incidunt a facilis nam optio repudiandae
-          ea debitis sed corrupti repellat autem doloremque. Id quos cumque
-          itaque minima voluptatum modi nisi nihil labore repellendus expedita
-          aliquid culpa neque soluta corporis quisquam, voluptatibus laborum
-          quia, fuga deleniti! Mollitia, laboriosam. Dicta atque aliquam
-          adipisci inventore illo nostrum quos necessitatibus nemo omnis placeat
-          aspernatur praesentium repudiandae vero modi eius, deserunt reiciendis
-          sapiente suscipit consequuntur. Ut ad veritatis fugit quos a totam
-          quibusdam sit cupiditate quidem laborum adipisci, accusantium quod
-          harum! Suscipit nemo repudiandae quibusdam, nam omnis eum nihil harum
-          ipsam error, expedita odio rem quae quos reiciendis corporis
-          reprehenderit, ab autem exercitationem temporibus et animi. Porro
-          quaerat necessitatibus mollitia ex modi totam velit soluta ab ratione
-          doloribus tenetur vero, asperiores exercitationem, rem est hic nemo
-          illo corrupti! Commodi dolorum, adipisci pariatur, laborum ratione
-          architecto optio possimus illum expedita earum eius minus. Dolores in
-          modi ratione amet doloribus voluptate. Corporis corrupti ipsa,
-          perferendis repellat obcaecati suscipit explicabo minima deleniti cum
-          saepe natus modi accusamus mollitia eos amet? Enim inventore sunt
-          eveniet, dolore culpa, rerum delectus maxime illo odit praesentium
-          minima quam iure voluptatem excepturi obcaecati! Laborum placeat ex
-          molestias, a at, quaerat porro dolor totam nulla dignissimos fugiat
-          harum praesentium, modi aperiam sed molestiae laudantium sint! Laborum
-          deleniti fugit sint excepturi nihil! Ipsam, repudiandae quam. Velit,
-          porro! Tenetur, corporis. In, suscipit facere, unde, odit cupiditate
-          neque ea officiis deleniti quo et sint! Placeat deserunt porro
-          deleniti cum ipsa nihil, fugit sint animi perspiciatis, doloremque
-          sit. Nobis distinctio, deserunt quaerat vel fugit, expedita magni qui
-          voluptatibus perferendis mollitia, iste cumque. Nesciunt velit tempora
-          totam cumque exercitationem praesentium at eaque in, sed rerum,
-          expedita esse rem culpa quidem ut. Eius ratione nostrum, excepturi
-          repudiandae non id quia consequuntur rem ipsum temporibus, iusto
-          accusamus. Hic illum culpa assumenda blanditiis autem. Nostrum
-          blanditiis voluptatem suscipit labore inventore error dolor. Vitae
-          excepturi porro cumque id repellat fugit eius error inventore, aperiam
-          exercitationem. Cupiditate sequi nam dolorem eligendi eum deleniti
-          mollitia magnam, qui hic perferendis quidem quod libero voluptates
-          laboriosam, accusantium aperiam quae veniam, ipsa neque ab deserunt
-          tenetur totam sit excepturi! Temporibus pariatur labore nihil quidem
-          ut, voluptatum sapiente dolores deserunt rerum facere tempore
-          accusamus vero, quaerat perspiciatis amet nisi ipsum repudiandae quo
-          laudantium dignissimos possimus iusto recusandae. Obcaecati nostrum
-          itaque earum! Suscipit error distinctio deleniti ab, vero assumenda,
-          veritatis, exercitationem nostrum necessitatibus unde temporibus
-          debitis totam! Debitis odio quia corrupti repellat ex earum saepe fuga
-          soluta? Beatae nemo vero earum dolore cumque ab molestias temporibus,
-          inventore in ex dolor autem. Officia.F
-        </p>
+        <p>{t("HomeAbout")}</p>
       </Box>
     </div>
   );
